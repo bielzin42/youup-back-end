7888813fcee91e6656a8f385bd605c06be2c694c
Bielzin42 <nickspoot@gmail.com>
2020-07-15 14:52:10 -0300

    uploading fresh start spring boot

---
+++ youupmain/src/main/java/com/youup/youupmain/YouupmainApplication.java
@@ -0,0 +1,13 @@
+package com.youup.youupmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouupmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouupmainApplication.class, args);
+	}
+
+}
---
+++ youupmain/src/test/java/com/youup/youupmain/YouupmainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youup.youupmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouupmainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
